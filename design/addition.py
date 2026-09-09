270d7c0449fffc4c4d5aae0ccd86778b6e892c9d
Kamtchoum Paul Kevyn <kamtchoum.kevyn@ictuniversity.edu.cm>
2026-09-09 14:09:15 +0100

    Add project folder structure for backend and frontend

---
+++ design/addition.py
@@ -0,0 +1,5 @@
+# addition of two numbers
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = a + b
+print("THE SUM OF {0} AND {1} IS : {2}".format(a, b, c))
